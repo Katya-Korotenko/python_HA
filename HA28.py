47371ae7dc743e7536a1ef670fabbf7eeea07ba9
Katya-Korotenko <korotenkok99943@gmail.com>
2026-04-20 21:56:46 +0200

    HA28 iterator and generator

---
+++ HA28.py
@@ -0,0 +1,79 @@
+# План по дням недели
+# Напишите программу, которая помогает планировать дела.
+# Программа должна бесконечно выводить план на следующий день недели,
+# пока пользователь нажимает 'Enter'.
+# Пример ввода:
+# Нажмите 'Enter' для получения плана:
+# Monday: Gym, Work, Read book
+# Нажмите 'Enter' для получения плана:
+# Tuesday: Meeting, Work, Study Python
+# Данные:
+from itertools import cycle
+# Расписание дел на неделю
+weekly_schedule = {
+    "Monday": ["Gym", "Work", "Read book"],
+    "Tuesday": ["Meeting", "Work", "Study Python"],
+    "Wednesday": ["Shopping", "Work", "Watch movie"],
+    "Thursday": ["Work", "Call parents", "Play guitar"],
+    "Friday": ["Work", "Dinner with friends"],
+    "Saturday": ["Hiking", "Rest"],
+    "Sunday": ["Family time", "Rest"]
+}
+
+def weekly_list(weekly_schedule):
+    schedule_cycle = cycle(weekly_schedule.items())
+    while True:
+        user_input = input("Нажмите 'Enter' для получения плана:")
+        day, tasks = next(schedule_cycle)
+        print(f"{day}: {', '.join(tasks)}")
+
+
+#weekly_list(weekly_schedule)
+
+
+
+# Объединение списков продуктов
+# Напишите функцию, которая принимает несколько списков с названиями продуктов и возвращает генератор,
+# содержащий все продукты в нижнем регистре.
+# Выведите содержимое генератора.
+# Данные:
+import itertools
+
+fruits = ["Apple", "Banana", "Orange"]
+vegetables = ["Carrot", "Tomato", "Cucumber"]
+dairy = ["Milk", "Cheese", "Yogurt"]
+
+# первый вариант
+# def products_list(*args):
+#     return (item.lower() for lst in args for item in lst)
+#
+# for item in products_list(fruits, vegetables, dairy):
+#     print(item)
+
+
+# второй варинат
+def products_list(fruits,vegetables,dairy):
+    merged = itertools.chain(fruits,vegetables,dairy)
+    return (item.lower() for item in merged)
+
+
+# for item in products_list(fruits, vegetables, dairy):
+#     print(item)
+
+
+# Комбинации одежды
+# Напишите функцию, которая принимает списки типов одежды, цветов и размеров,
+# а затем генерирует все возможные комбинации
+# в формате "Clothe - Color - Size".
+# Данные:
+
+clothes = ["T-shirt", "Jeans", "Jacket"]
+colors = ["Red", "Blue", "Black"]
+sizes = ["S", "M", "L"]
+
+def combination(clothes,colors,sizes):
+    pairs = itertools.product(clothes,colors,sizes)
+    return pairs
+
+for item in combination(clothes,colors,sizes):
+    print("-".join(item))
